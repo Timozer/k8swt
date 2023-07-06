4a853d766650baf2231990a617169928f2cd2d23
Timozer <zhenyuwang94@gmail.com>
2023-07-06 13:24:53 +0000

    add build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo 'please input the docker version'
+    exit 1
+fi
+
 CGO_ENABLED=0 go build -ldflags="-s -w" -o k8swt
 
-sudo docker buildx build --output type=docker -t k8swt:latest .
+sudo docker buildx build --output type=docker -t 878592748/k8swt:latest .
+
+sudo docker tag 878592748/k8swt:latest 878592748/k8swt:$1
+
+sudo docker push 878592748/k8swt:latest
+sudo docker push 878592748/k8swt:$1
